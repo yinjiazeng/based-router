90eb6958ffba738aad707e1be14b9056a07c87f7
yinjiazeng <yinjiazeng@163.com>
2020-07-01 18:24:21 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # based-router
 一个简单的路由器
+
+```js
+import { create, replace } from 'based-router';
+
+const router = create({ type: 'hash', basename: '/' });
+
+router.get('/', (location) => {
+  // do something
+});
+
+router.get('/home', (location) => {
+  // do something
+});
+
+router.get('/detail/:id', (location) => {
+  // do something
+});
+
+router.get('*', () => {
+  replace('/');
+});
+```
---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+import isString from 'lodash/isString';
+import isObjectLike from 'lodash/isObjectLike';
+import isFunction from 'lodash/isFunction';
+import noop from 'lodash/noop';
+
+export const globalWindow = typeof window !== 'undefined' && typeof window.document !== 'undefined'
+? window
+: {
+  addEventListener: noop,
+  removeEventListener: noop,
+  location: {
+    pathname: '',
+    search: '',
+    hash: '',
+    replace: noop,
+  },
+  history: {
+    pushState: noop,
+    replaceState: noop,
+  },
+};
+
+export {
+  isString,
+  isObjectLike,
+  isFunction,
+  noop,
+}
